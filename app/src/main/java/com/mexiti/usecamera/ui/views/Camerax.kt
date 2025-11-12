5218b10b9f39a8568291e2ce9b5fba1e23fc4503
faraday58 <jarodriguezvera@gmail.com>
2025-11-11 20:59:38 -0600

    Add configuración para salvar fotografía.

--- app/src/main/java/com/mexiti/usecamera/ui/views/Camerax.kt
+++ app/src/main/java/com/mexiti/usecamera/ui/views/Camerax.kt
@@ -1,8 +1,13 @@
 package com.mexiti.usecamera.ui.views
 
+import android.content.ContentValues
 import android.content.Context
+import android.os.Build
+import android.provider.MediaStore
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
+import androidx.camera.core.takePicture
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.fillMaxSize
@@ -55,3 +60,26 @@ private suspend fun Context.getCameraProvider() : ProcessCameraProvider =
         }
    }
 
+private fun captureImage(imageCapture: ImageCapture, context: Context ){
+
+    val name = "CameraxImage.jpeg"
+    val contentValues = ContentValues().apply {
+        put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+        put(MediaStore.MediaColumns.MIME_TYPE,"image/jpeg")
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P){
+            put(MediaStore.Images.Media.RELATIVE_PATH,"Pictures/CameraX-Image")
+        }
+    }
+    val outputOptions = ImageCapture.OutputFileOptions
+        .Builder(
+            context.contentResolver,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            contentValues
+        )
+        .build()
+
+    imageCapture.takePicture(
+
+    )
+
+}
